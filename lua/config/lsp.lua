79a1278812b9e754518e723be1018db655979996
jad <assaf.jad230@gmail.com>
2026-01-11 17:16:46 -0500

    Update lsp.lua

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,11 +1,11 @@
 ﻿local lspconfig = require("lspconfig")
 
--- C / C++
+
 lspconfig.clangd.setup({
   cmd = { "clangd", "--background-index" },
 })
 
--- Lua (for Neovim config itself)
+
 lspconfig.lua_ls.setup({
   settings = {
     Lua = {
