e4075397a3fc5199eee9a7c9135da641894f8168
jad <assaf.jad230@gmail.com>
2026-01-31 23:37:52 -0500

    Clean up init.lua and resolve merge conflicts
    Removed merge conflict markers and cleaned up the init.lua file.

--- init.lua
+++ init.lua
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-
-=======
-
->>>>>>> e597410 (yeah yeah rewrote configs)
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-
 require("options")
-require("keymaps")
-
-
 require("lazy").setup("plugins")
-
+require("keymaps")
